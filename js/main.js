2b85d796b3ecf0303a00828e28f3d267db39d1a4
David Nelsen <davethenet@users.noreply.github.com>
2025-04-27 16:29:49 +0300

    Add download feature

--- js/main.js
+++ js/main.js
@@ -22,26 +22,7 @@ function renderMeme(meme){
     drawImg(meme)
 }
 
-function drawImg(meme) {
-    const img = new Image()
-    img.src = `img/${meme.selectedImgId}.jpg`
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-        drawText(meme.lines[0].txt, gElCanvas.width / 2, gElCanvas.height / 2, meme)
-
-    }
+function onDownloadImg(elLink) {
+    const imgContent = gElCanvas.toDataURL('image/jpeg')
+    elLink.href = imgContent
 }
-
-
-function drawText(text, x, y, meme) {
-    gCtx.lineWidth = 2
-    gCtx.strokeStyle = meme.lines[0].color
-    gCtx.fillStyle = meme.lines[0].color
-    gCtx.font = meme.lines[0].size + 'px Ariel'
-    gCtx.textAlign = 'center'
-    gCtx.textBaseline = 'middle'
-  
-    gCtx.fillText(text, x, y)
-    gCtx.strokeText(text, x, y)
-  }
-
