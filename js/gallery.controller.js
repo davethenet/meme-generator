4840f89394a974051c6f0ee8fb0c728e692436d2
David Nelsen <davethenet@users.noreply.github.com>
2025-04-27 15:17:40 +0300

    Add setImage functionality

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,13 +1,21 @@
 'use strict'
 
+var gImgId = 1
+
 function renderGallery(){
     const elGallery = document.querySelector('.gallery')
     console.log(elGallery)
     var imgs = getImgs()
     elGallery.innerHTML = imgs.map(img =>
         `<tr>
-                <td><img src=${img.url}></td>
+                <td><img src="${img.url}" id="${gImgId++}" onclick="onImgSelect(this)"></td>
                 
             </tr>`
     ).join('')
+}
+
+
+function onImgSelect(img) {
+    console.log(img)
+    setImg(img)
 }
--- js/meme.service.js
+++ js/meme.service.js
@@ -35,3 +35,35 @@ function setLineTxt(){
 function getImgs(){
     return gImgs
 }
+
+function setImg(img){
+    console.log(img.id)
+    gMeme.selectedImgId = img.id
+    renderMeme(gMeme)
+}
+
+
+
+function drawImg(meme) {
+    const img = new Image()
+    img.src = `img/${meme.selectedImgId}.jpg`
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(meme.lines[0].txt, gElCanvas.width / 2, gElCanvas.height / 2, meme)
+
+    }
+}
+
+
+function drawText(text, x, y, meme) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = meme.lines[0].color
+    gCtx.fillStyle = meme.lines[0].color
+    gCtx.font = meme.lines[0].size + 'px Ariel'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+  
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+  }
+
